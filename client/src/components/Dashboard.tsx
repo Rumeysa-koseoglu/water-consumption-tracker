2735b8a9fe539d6516c8c1b5035b2524c349def7
Rumeysa-koseoglu <koseoglurumeysa0@gmail.com>
2026-02-26 23:09:18 +0300

    chore: add base components

--- client/src/components/Dashboard.tsx
+++ client/src/components/Dashboard.tsx
@@ -1,5 +1,21 @@
+import DailyEntry from "./DailyEntry";
+import DailyTotal from "./DailyTotal";
+import DailyUsageByCategory from "./DailyUsageByCategory";
+import DailyUsageList from "./DailyUsageList";
+import Header from "./Header";
+import WeeklyUsageSummary from "./WeeklyUsageSummary";
+
 const Dashboard: React.FC = () => {
-  return <div className="text-red-600">fsdfsfd</div>;
+  return (
+    <div className="w-screen h-screen flex flex-col">
+      <Header />
+      <DailyEntry />
+      <DailyTotal />
+      <DailyUsageByCategory />
+      <DailyUsageList />
+      <WeeklyUsageSummary />
+    </div>
+  );
 };
 
 export default Dashboard;
