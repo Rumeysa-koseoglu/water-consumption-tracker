8fbbfa38f804acb7d33444524b5e6db32c8ee364
Rumeysa-koseoglu <koseoglurumeysa0@gmail.com>
2026-03-03 16:41:40 +0300

    feat: add edit and delete entry functionalities

--- client/src/components/Dashboard.tsx
+++ client/src/components/Dashboard.tsx
@@ -7,6 +7,7 @@ import Header from "./Header";
 
 const Dashboard: React.FC = () => {
   const [entries, setEntries] = useState<any[]>([]);
+  const [editEntry, setEditEntry] = useState<any | null>(null);
 
   const fetchEntries = async () => {
     try {
@@ -33,17 +34,46 @@ const Dashboard: React.FC = () => {
     return acc;
   }, {});
 
+  const deleteEntry = async (id: string) => {
+    if (!window.confirm("Are you sure you want to delete this?")) return;
+
+    try {
+      const res = await fetch(
+        `http://localhost:5500/api/water/delete-entry/${id}`,
+        {
+          method: "DELETE",
+        }
+      );
+
+      if (res.ok) fetchEntries();
+    } catch (err) {
+      console.error("Delete Failed");
+    }
+  };
+
+  const handleEdit = (entry: any) => {
+    setEditEntry(entry);
+    document
+      .getElementById("entry-container")
+      ?.scrollIntoView({ behavior: "smooth" });
+  };
+
   return (
     <div className="w-screen h-screen flex flex-col font-outfit p-4 overflow-auto">
       <Header />
-      <div className="p-8 grid grid-cols-2 gap-6 you're tiny Chick-fil-A W psycho">
-        <DailyEntry onEntryAdded={fetchEntries} />
+      <div className="p-8 grid grid-cols-2 gap-6">
+        <DailyEntry
+          onEntryAdded={fetchEntries}
+          editEntry={editEntry}
+          setEditEntry={setEditEntry}
+        />
         <DailyTotal total={totalLiters} limit={150} />
         <DailyUsageByCategory categories={categoryTotals} />
-      </div>
-
-      <div className="md:col-span-2">
-        <DailyUsageList entries={entries} />
+        <DailyUsageList
+          entries={entries}
+          onDelete={deleteEntry}
+          onEdit={handleEdit}
+        />
       </div>
     </div>
   );
