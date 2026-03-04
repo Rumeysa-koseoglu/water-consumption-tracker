87ceb5a2f2fe3f47df1e5bf1beb3762eb5614a1e
Rumeysa-koseoglu <koseoglurumeysa0@gmail.com>
2026-03-04 05:45:27 +0300

    style: enhnce DailyTotal component UI

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,13 +1,13 @@
 "./App.css";
 import LoginPage from "./components/LoginPage";
 import { Routes, Route } from "react-router-dom";
-import Dashboard from './components/Dashboard'
+import Dashboard from "./components/Dashboard";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<LoginPage />} />
-      <Route path="/dashboard" element={< Dashboard/>} />
+      <Route path="/dashboard" element={<Dashboard />} />
     </Routes>
   );
 }
--- client/src/components/DailyTotal.tsx
+++ client/src/components/DailyTotal.tsx
@@ -3,25 +3,35 @@ const DailyTotal = ({ total, limit }: { total: number; limit: number }) => {
 
   return (
     <div
-      className={`p-6 rounded-xl border-2 ${
+      className={`p-4 rounded-xl border-2 ${
         isOverLimit
           ? "border-red-500 bg-red-900/20"
           : "bg-[#243046] border-transparent"
       }`}
     >
-      <h3 className="text-gray-400 text-sm font-bold uppercase">Total Usage</h3>
-      <p
-        className={`text-4xl font-bold ${
-          isOverLimit ? "text-red-500" : "text-[#79bef5]"
+      <h3
+        className={` mb-4 text-lg font-bold uppercase ${
+          isOverLimit ? "text-red-500" : "text-[#9ec8fb]"
         }`}
       >
-        {total.toFixed(2)} L
-      </p>
-      {isOverLimit && (
-        <p className="text-red-400 text-sm mt-2 font-bold animate-pulse">
-          You have exceeded your {limit}L limit!
+        Total Usage
+      </h3>
+      <div className="flex flex-col items-start justify-center gap-10 p-4">
+        <p
+          className={`text-5xl font-bold ${
+            isOverLimit ? "text-red-500" : "text-[#79bef5]"
+          }`}
+        >
+          {total.toFixed(2)} L
         </p>
-      )}
+        {isOverLimit ? (
+          <p className="text-red-400 text-3xl mt-2 font-bold animate-pulse">
+            You have exceeded your {limit}L limit!
+          </p>
+        ) : (
+          <p className=" text-[#9fbd92] text-3xl">(Your daily limit is 150L)</p>
+        )}
+      </div>
     </div>
   );
 };
