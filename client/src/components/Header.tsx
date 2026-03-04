f8c81c996686c948aa1c148b31277e57d03d4bd3
Rumeysa-koseoglu <koseoglurumeysa0@gmail.com>
2026-03-04 05:40:06 +0300

    style: new sizing and color adjusments - readjust color and size of logo (watrack) - readjust Logout button's sizing - add bottom border

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -2,17 +2,18 @@ import { CircleUserRound } from "lucide-react";
 
 const Header: React.FC = () => {
   return (
-    <div className=" w-full h-20 flex flex-row items-center justify-between p-4 sticky">
+    <div className=" w-full bg-[#182334] h-20 flex flex-row items-center justify-between p-6 sticky top-0 z-0 mb-5 border-b border-b-[#21314d] shadow">
       <p
-        className=" md:flex text-4xl lg:text-5xl  text-center text-[#84b0d3]
+        className=" md:flex text-4xl lg:text-4xl  text-center text-[#9ec8fb]
        font-bold font-bungee word-spacing-sm md:word-spacing"
       >
         WA
-        <span className=" text-5xl lg:text-6xl text-[#13325b]">T</span>RACK
+        <span className=" text-5xl lg:text-5xl text-[#13325b]">T</span>RACK
       </p>
 
-      <button className="flex flex-row gap-3 items-center bg-[#2a507d] text-lg rounded-4xl py-2.5 pl-3 pr-4">
-        <CircleUserRound className="text-[#13325b] size-7" /> Log out
+      <button className="flex flex-row gap-3 items-center bg-[#2a507d] text-base rounded-4xl py-2 pl-3 pr-5">
+        <CircleUserRound className="text-[#13325b] size-6" />
+        <span className="text-[#bed6f5]">Log out</span>
       </button>
     </div>
   );
