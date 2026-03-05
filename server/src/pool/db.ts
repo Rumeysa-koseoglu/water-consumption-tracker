2972652151536cb2e225f182d287dd5f8375d95e
Rumeysa-koseoglu <koseoglurumeysa0@gmail.com>
2026-03-05 15:32:21 +0300

    refactor: update pool

--- server/src/pool/db.ts
+++ server/src/pool/db.ts
@@ -2,16 +2,17 @@ import dotenv from "dotenv";
 dotenv.config();
 import { Pool } from "pg";
 
-export const pool = new Pool({
-  user: process.env.DB_USER,
-  password: String(process.env.DB_PASSWORD),
-  host: process.env.DB_HOST,
-  port: Number(process.env.DB_PORT) || 5432,
-  database: process.env.DB_NAME,
-  ssl:
-    process.env.NODE_ENV === "production"
-      ? { rejectUnauthorized: false }
-      : false,
-});
+export const pool = process.env.DATABASE_URL
+  ? new Pool({
+      connectionString: process.env.DATABASE_URL,
+      ssl: { rejectUnauthorized: false },
+    })
+  : new Pool({
+      user: process.env.DB_USER,
+      password: String(process.env.DB_PASSWORD),
+      host: process.env.DB_HOST,
+      port: Number(process.env.DB_PORT) || 5432,
+      database: process.env.DB_NAME,
+    });
 
 export const query = (text: string, params?: any[]) => pool.query(text, params);
