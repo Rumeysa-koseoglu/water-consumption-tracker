78eb83330a3817a6f234a32bafd2526ad349d5d5
Rumeysa-koseoglu <koseoglurumeysa0@gmail.com>
2026-03-05 15:17:24 +0300

    feat: add ssl config

--- server/src/pool/db.ts
+++ server/src/pool/db.ts
@@ -3,10 +3,15 @@ dotenv.config();
 import { Pool } from "pg";
 
 export const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-  ssl: {
-    rejectUnauthorized: false,
-  },
+  user: process.env.DB_USER,
+  password: String(process.env.DB_PASSWORD),
+  host: process.env.DB_HOST,
+  port: Number(process.env.DB_PORT) || 5432,
+  database: process.env.DB_NAME,
+  ssl:
+    process.env.NODE_ENV === "production"
+      ? { rejectUnauthorized: false }
+      : false,
 });
 
 export const query = (text: string, params?: any[]) => pool.query(text, params);
